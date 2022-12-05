712f9c52012216973acaa2c265e300676efcecd4
akbar <riegerlukas1945@gmail.com>
2022-12-05 07:12:07 +0700

    - Delayed restart

--- java/com/example/tugas/MainActivity3.java
+++ java/com/example/tugas/MainActivity3.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -66,7 +67,12 @@ public class MainActivity3 extends AppCompatActivity {
                 String hintLebih = "Lebih dari " + angka;
                 if (randomChoice == angka){
                     hasil.setText(won);
-                    restart();
+                    Handler handler = new Handler();
+                    handler.postDelayed(new Runnable() {
+                        public void run() {
+                            restart();
+                        }
+                    }, 2000);
                 } else if (angka < randomChoice){
                     coba[0]++;
                     hasil.setText(hintLebih);
